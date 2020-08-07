c0c37e573843299d9211e2db624668158ffbdf00
Maurilio David S. Conceicao <maurilio_david@hotmail.com>
2020-08-06 21:18:59 -0300

    correção no config

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
 ? 'http://localhost:8080'
-: 'https://https://glflix.herokuapp.com';
+: 'https://glflix.herokuapp.com';
 
 export default {
 URL_BACKEND_TOP,
